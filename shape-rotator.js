496c8e63ed8e49b7849987709b74ac3482d945f5
Nate Goldman <ungoldman@gmail.com>
2022-04-13 13:31:57 -0700

    improve code somewhat

--- shape-rotator.js
+++ shape-rotator.js
@@ -3,6 +3,7 @@
 // I mostly added pointer interactivity to allow the user to rotate,
 // then spent time fiddling with colors and and composite ops.
 
+// note: will currently break if window is moved to display w/ different DPR
 const DPR = window.devicePixelRatio || 1
 const CANCEL_EVENTS = ['pointerup', 'pointerout', 'pointerleave', 'pointercancel']
 const COMPOSITE_OPS = [
@@ -34,26 +35,14 @@ const COMPOSITE_OPS = [
   'luminosity'
 ]
 
-// adapted from https://www.html5rocks.com/en/tutorials/canvas/hidpi/
-// mutates canvas & ctx
-function setCanvasDimensions (canvas, ctx) {
-  // Get the device pixel ratio, falling back to 1.
-  // Get the size of the canvas in CSS pixels.
-  const rect = canvas.getBoundingClientRect()
-  // Give the canvas pixel dimensions of their CSS
-  // size * the device pixel ratio.
-  canvas.width = rect.width * DPR
-  canvas.height = rect.height * DPR
+class Point3D {
+  constructor (x, y, z) {
+    this.x = x
+    this.y = y
+    this.z = z
+  }
 
-  ctx.scale(DPR, DPR)
-}
-
-function Point3D (x, y, z) {
-  this.x = x
-  this.y = y
-  this.z = z
-
-  this.rotateX = function (currentAngle) {
+  rotateX (currentAngle) {
     const rad = (currentAngle * Math.PI) / 180
     const cosa = Math.cos(rad)
     const sina = Math.sin(rad)
@@ -63,7 +52,7 @@ function Point3D (x, y, z) {
     return new Point3D(this.x, y, z)
   }
 
-  this.rotateY = function (currentAngle) {
+  rotateY (currentAngle) {
     const rad = (currentAngle * Math.PI) / 180
     const cosa = Math.cos(rad)
     const sina = Math.sin(rad)
@@ -73,7 +62,7 @@ function Point3D (x, y, z) {
     return new Point3D(x, this.y, z)
   }
 
-  this.rotateZ = function (currentAngle) {
+  rotateZ (currentAngle) {
     const rad = (currentAngle * Math.PI) / 180
     const cosa = Math.cos(rad)
     const sina = Math.sin(rad)
@@ -83,7 +72,7 @@ function Point3D (x, y, z) {
     return new Point3D(x, y, this.z)
   }
 
-  this.project = function (viewWidth, viewHeight, fieldOfView, viewDistance) {
+  project (viewWidth, viewHeight, fieldOfView, viewDistance) {
     const factor = (fieldOfView / DPR) / (viewDistance + this.z)
     const x = this.x * factor + (viewWidth / DPR) / 2
     const y = this.y * factor + (viewHeight / DPR) / 2
@@ -91,7 +80,21 @@ function Point3D (x, y, z) {
   }
 }
 
-let vertices = [
+const canvas = document.querySelector('#canvas')
+const ctx = canvas.getContext('2d')
+
+const state = {
+  canvas,
+  ctx,
+
+  lastX: 0,
+  lastY: 0,
+
+  dirX: 0.1,
+  dirY: -0.1
+}
+
+state.vertices = [
   new Point3D(-1, 1, -1),
   new Point3D(1, 1, -1),
   new Point3D(1, -1, -1),
@@ -99,7 +102,7 @@ let vertices = [
   new Point3D(-1, 1, 1),
   new Point3D(1, 1, 1),
   new Point3D(1, -1, 1),
-  new Point3D(-1, -1, 1),
+  new Point3D(-1, -1, 1)
 ]
 
 const cubeFaces = [
@@ -108,24 +111,25 @@ const cubeFaces = [
   [5, 4, 7, 6],
   [4, 0, 3, 7],
   [0, 4, 5, 1],
-  [3, 2, 6, 7],
+  [3, 2, 6, 7]
 ]
 
-function r (n) { return Math.round(Math.random() * n) }
-
-const colors = Array(24).fill(0).map(c => {
+const colors = Array(24).fill(0).map(_ => {
   return `rgba(${r(255)},${r(255)},${r(255)}, 0.9)`
 })
 
-const ops = Array(6).fill(0).map(o => {
-  return COMPOSITE_OPS[r(25)]
+const lastn = []
+const ops = Array(6).fill(0).map(_ => {
+  let n = r(25)
+  while (lastn.includes(n)) n = r(25) // ensure unique op for every face
+  lastn.push(n)
+  return COMPOSITE_OPS[n]
 })
 
-function startRendering () {
-  const points = this.points = []
-  const canvas = this.canvas = document.querySelector('#canvas')
-  const ctx = canvas.getContext('2d')
+console.log(lastn)
 
+function startRendering () {
+  const { canvas, ctx } = state
   setCanvasDimensions(canvas, ctx)
 
   this.handleEvent = handleEvent.bind(this)
@@ -152,78 +156,93 @@ function startRendering () {
     })
   })
 
-  setInterval(() => renderLoop(points, canvas, ctx), 50)
+  setInterval(() => renderLoop(canvas, ctx), 50)
 }
 
-function renderLoop (points, canvas, ctx) {
-
-
+function renderLoop (canvas, ctx) {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
   this.handleEvent(null, true)
 
-  vertices.forEach((vertex, i) => {
-    this.points[i] = vertex.project(canvas.width, canvas.height, canvas.height, 6)
+  const points = state.vertices.map(vertex => {
+    return vertex.project(canvas.width, canvas.height, canvas.height, 6)
   })
 
   let i = 0
 
-  cubeFaces.map((cubeFace, i) => {
+  cubeFaces.forEach((cubeFace, j) => {
     ctx.beginPath()
     ctx.moveTo(points[cubeFace[0]].x, points[cubeFace[0]].y)
-    ctx.strokeStyle = colors[i]
+    ctx.strokeStyle = colors[i++]
     ctx.lineTo(points[cubeFace[1]].x, points[cubeFace[1]].y)
-    ctx.strokeStyle = colors[i + 1]
+    ctx.strokeStyle = colors[i++]
     ctx.lineTo(points[cubeFace[2]].x, points[cubeFace[2]].y)
-    ctx.strokeStyle = colors[i + 2]
+    ctx.strokeStyle = colors[i++]
     ctx.lineTo(points[cubeFace[3]].x, points[cubeFace[3]].y)
     ctx.closePath()
     ctx.stroke()
-    ctx.globalCompositeOperation = ops[i]
-    ctx.fillStyle = colors[i + 3]
+    ctx.globalCompositeOperation = ops[j]
+    ctx.fillStyle = colors[i++]
     ctx.fill()
     ctx.globalCompositeOperation = 'source-over'
   })
 }
 
-let lastX = 0
-let lastY = 0
-
-let dirX = 0.1
-let dirY = -0.1
-
 function handleEvent (event, drift) {
   if (event) event.preventDefault()
-  let { clientX, clientY } = event || {}
+  const { clientX, clientY } = event || {}
   let x = 0
   let y = 0
-  let z = 0
+  // let z = 0
 
-  if ([clientX, lastX].every(s => typeof s === 'number')) {
+  if (clientX != null && state.lastX != null) {
     // clientX = Math.round(clientX / 20) * 20
     // clientY = Math.round(clientY / 20) * 20
     // console.log(clientX - lastX, clientY - lastY)
-    x = (clientY - lastY)
-    y = (lastX - clientX)
-    dirX = !!x ? 0.1 : -0.1
-    dirY = !!y ? 0.1 : -0.1
+    x = (clientY - state.lastY)
+    y = (state.lastX - clientX)
+    state.dirX = x ? 0.1 : -0.1
+    state.dirY = y ? 0.1 : -0.1
     // z++
   } else if (drift) {
     // console.log(dirX, dirY)
-    x += dirX
-    y += dirY
+    x += state.dirX
+    y += state.dirY
     // z += dirY
   }
 
-  vertices = vertices.map((vertex, i) => {
+  state.vertices = state.vertices.map((vertex, i) => {
     return vertex
       .rotateX(x)
       .rotateY(y)
-      .rotateZ(z)
+      // .rotateZ(z)
   })
 
-  lastX = clientX
-  lastY = clientY
+  state.lastX = clientX
+  state.lastY = clientY
+}
+
+// helper fns
+
+/**
+ * generate random number
+ * @param {number} n - upper limit
+ * @returns number
+ */
+function r (n) { return Math.round(Math.random() * n) }
+
+// adapted from https://www.html5rocks.com/en/tutorials/canvas/hidpi/
+// mutates canvas & ctx
+function setCanvasDimensions (canvas, ctx) {
+  // Get the device pixel ratio, falling back to 1.
+  // Get the size of the canvas in CSS pixels.
+  const rect = canvas.getBoundingClientRect()
+  // Give the canvas pixel dimensions of their CSS
+  // size * the device pixel ratio.
+  canvas.width = rect.width * DPR
+  canvas.height = rect.height * DPR
+
+  ctx.scale(DPR, DPR)
 }
 
 window.onload = startRendering
