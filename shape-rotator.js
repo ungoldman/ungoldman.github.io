80d34ecd1df830eece75b9f3b88c535f1349643e
Nate Goldman <ungoldman@gmail.com>
2022-04-13 11:50:30 -0700

    randomize global composite operation per cube face

--- shape-rotator.js
+++ shape-rotator.js
@@ -1,5 +1,33 @@
 const DPR = window.devicePixelRatio || 1
 const CANCEL_EVENTS = ['pointerup', 'pointerout', 'pointerleave', 'pointercancel']
+const COMPOSITE_OPS = [
+  'source-over',
+  'source-in',
+  'source-out',
+  'source-atop',
+  'destination-over',
+  'destination-in',
+  'destination-out',
+  'destination-atop',
+  'lighter',
+  'copy',
+  'xor',
+  'multiply',
+  'screen',
+  'overlay',
+  'darken',
+  'lighten',
+  'color-dodge',
+  'color-burn',
+  'hard-light',
+  'soft-light',
+  'difference',
+  'exclusion',
+  'hue',
+  'saturation',
+  'color',
+  'luminosity'
+]
 
 // adapted from https://www.html5rocks.com/en/tutorials/canvas/hidpi/
 // mutates canvas & ctx
@@ -81,7 +109,11 @@ const cubeFaces = [
 function r (n) { return Math.round(Math.random() * n) }
 
 const colors = Array(24).fill(0).map(c => {
-  return `rgba(${r(255)},${r(255)},${r(255)}, 0.5)`
+  return `rgba(${r(255)},${r(255)},${r(255)}, 0.9)`
+})
+
+const ops = Array(6).fill(0).map(o => {
+  return COMPOSITE_OPS[r(25)]
 })
 
 function startRendering () {
@@ -95,6 +127,14 @@ function startRendering () {
 
   this.handleEvent(null)
 
+  // prevent all touch events
+  canvas.addEventListener('touchstart', event => event.preventDefault())
+  canvas.addEventListener('touchmove', event => event.preventDefault())
+  canvas.addEventListener('touchend', event => event.preventDefault())
+  canvas.addEventListener('touchcancel', event => event.preventDefault())
+
+  // capture & handle pointer events (should cover mouse & touch)
+  // note: apparently you still need to handle touch* when using pointer*
   canvas.addEventListener('pointerdown', event => {
     event.preventDefault()
     canvas.addEventListener('pointermove', this.handleEvent)
@@ -126,16 +166,18 @@ function renderLoop (points, canvas, ctx) {
   cubeFaces.map((cubeFace, i) => {
     ctx.beginPath()
     ctx.moveTo(points[cubeFace[0]].x, points[cubeFace[0]].y)
-    ctx.strokeStyle = colors[i++]
+    ctx.strokeStyle = colors[i]
     ctx.lineTo(points[cubeFace[1]].x, points[cubeFace[1]].y)
-    ctx.strokeStyle = colors[i++]
+    ctx.strokeStyle = colors[i + 1]
     ctx.lineTo(points[cubeFace[2]].x, points[cubeFace[2]].y)
-    ctx.strokeStyle = colors[i++]
+    ctx.strokeStyle = colors[i + 2]
     ctx.lineTo(points[cubeFace[3]].x, points[cubeFace[3]].y)
     ctx.closePath()
     ctx.stroke()
-    ctx.fillStyle = colors[i++]
+    ctx.globalCompositeOperation = ops[i]
+    ctx.fillStyle = colors[i + 3]
     ctx.fill()
+    ctx.globalCompositeOperation = 'source-over'
   })
 }
 
@@ -155,14 +197,14 @@ function handleEvent (event, drift) {
   if ([clientX, lastX].every(s => typeof s === 'number')) {
     // clientX = Math.round(clientX / 20) * 20
     // clientY = Math.round(clientY / 20) * 20
-    console.log(clientX - lastX, clientY - lastY)
+    // console.log(clientX - lastX, clientY - lastY)
     x = (clientY - lastY)
     y = (lastX - clientX)
     dirX = !!x ? 0.1 : -0.1
     dirY = !!y ? 0.1 : -0.1
     // z++
   } else if (drift) {
-    console.log(dirX, dirY)
+    // console.log(dirX, dirY)
     x += dirX
     y += dirY
     // z += dirY
